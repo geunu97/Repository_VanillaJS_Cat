1a68ac5e6585a2136eb8de6727eb02f80ee7698b
olo90632951 <olo90632951@gmail.com>
2022-04-12 15:21:53 +0900

    input autofocus 적용

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -5,6 +5,7 @@ export default class SearchInput {
     const $searchInput = document.createElement("input");
     this.$searchInput = $searchInput;
     this.$searchInput.placeholder = "고양이를 검색해보세요.|";
+    this.$searchInput.autofocus = true;
 
     $searchInput.className = "SearchInput";
     $target.appendChild($searchInput);
@@ -13,6 +14,10 @@ export default class SearchInput {
       if (e.keyCode === 13) {
         onSearch(e.target.value);
       }
+    });
+
+    $searchInput.addEventListener("click", (e) => {
+      if (e.target.value.length > 0) e.target.value = "";
     });
 
     console.log("SearchInput created.", this);
