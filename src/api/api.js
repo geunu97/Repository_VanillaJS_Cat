ac89584e0549897b467f6cde4c387c43533bce06
olo90632951 <olo90632951@gmail.com>
2022-04-12 15:21:29 +0900

    api 에러처리

--- src/api/api.js
+++ src/api/api.js
@@ -3,12 +3,17 @@ const API_ENDPOINT =
 
 const api = {
   fetchCats: (keyword) => {
-    return fetch(`${API_ENDPOINT}/api/cats/search?q=${keyword}`).then((res) =>
-      res.json()
-    );
+    return fetch(`${API_ENDPOINT}/api/cats/search?q=${keyword}`).then((res) => {
+      if (res.status === 200) return res.json();
+      else console.log("에러가 발생했습니다");
+    });
   },
+
   fetchCatDetails: (id) => {
-    return fetch(`${API_ENDPOINT}/api/cats/${id}`).then((res) => res.json());
+    return fetch(`${API_ENDPOINT}/api/cats/${id}`).then((res) => {
+      if (res.status === 200) return res.json();
+      else console.log("에러가 발생했습니다");
+    });
   },
 };
 
