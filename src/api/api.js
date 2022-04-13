d098ede5accb13d0b8d7f0359298164d4b619fc0
olo90632951 <olo90632951@gmail.com>
2022-04-13 14:50:53 +0900

    await/asnc 변환

--- src/api/api.js
+++ src/api/api.js
@@ -2,25 +2,22 @@ const API_ENDPOINT =
   "https://oivhcpn8r9.execute-api.ap-northeast-2.amazonaws.com/dev";
 
 const api = {
-  fetchCats: (keyword) => {
-    return fetch(`${API_ENDPOINT}/api/cats/search?q=${keyword}`).then((res) => {
-      if (res.status === 200) return res.json();
-      else alert("에러가 발생했습니다. 다시 입력해 주세요");
-    });
+  fetchCats: async (keyword) => {
+    const res = await fetch(`${API_ENDPOINT}/api/cats/search?q=${keyword}`);
+    if (res.status === 200) return res.json();
+    else alert("에러가 발생했습니다. 다시 입력해 주세요");
   },
 
-  fetchCatDetails: (id) => {
-    return fetch(`${API_ENDPOINT}/api/cats/${id}`).then((res) => {
-      if (res.status === 200) return res.json();
-      else alert("에러가 발생했습니다. 다시 클릭해 주세요");
-    });
+  fetchCatDetails: async (id) => {
+    const res = await fetch(`${API_ENDPOINT}/api/cats/${id}`);
+    if (res.status === 200) return res.json();
+    else alert("에러가 발생했습니다. 다시 클릭해 주세요");
   },
 
-  fetchCatRandom: () => {
-    return fetch(`${API_ENDPOINT}/api/cats/random50`).then((res) => {
-      if (res.status === 200) return res.json();
-      else alert("에러가 발생했습니다. 다시 클릭해 주세요");
-    });
+  fetchCatRandom: async () => {
+    const res = await fetch(`${API_ENDPOINT}/api/cats/random50`);
+    if (res.status === 200) return res.json();
+    else alert("에러가 발생했습니다. 다시 클릭해 주세요");
   },
 };
 
